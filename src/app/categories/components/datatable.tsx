f3de395478c0593600d4bfe3d5e3da3c9d11da2f
iwindd <freewinendlife@gmail.com>
2025-06-08 19:19:24 +0700

    feat(category-datatable): add confirmation dialog properties for delete action

--- src/app/categories/components/datatable.tsx
+++ src/app/categories/components/datatable.tsx
@@ -34,6 +34,10 @@ const CategoryDatatable = () => {
   const confirmation = useConfirm({
     title: "แจ้งเตือน",
     text: "คุณต้องการที่จะลบประเภทสินค้าหรือไม่",
+    confirmProps: {
+      color: "error",
+      startIcon: <DeleteTwoTone />,
+    },
     onConfirm: async (id: number) => {
       try {
         const resp = await DeleteCategory(id);
