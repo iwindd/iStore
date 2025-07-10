b554bc9b3d1f1726c29fd31e6c27dd4823f1f799
iwindd <freewinendlife@gmail.com>
2025-07-10 16:42:11 +0700

    feat(borrow): add permission check for borrow creation in AddController component

--- src/app/borrows/components/add-controller.tsx
+++ src/app/borrows/components/add-controller.tsx
@@ -7,10 +7,15 @@ import { AddTwoTone } from "@mui/icons-material";
 import { useInterface } from "@/providers/InterfaceProvider";
 import { useDialog } from "@/hooks/use-dialog";
 import BorrowDialogForm from "./borrowDialogForm";
+import { useAuth } from "@/hooks/use-auth";
+import { BorrowPermissionEnum } from "@/enums/permission";
 
 const AddController = () => {
   const dialog = useDialog();
   const { isBackdrop } = useInterface();
+  const { user } = useAuth();
+
+  if (!user?.hasPermission(BorrowPermissionEnum.CREATE)) return null;
 
   return (
     <>
