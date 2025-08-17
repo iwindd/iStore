876d6a8803f77d8f9a45f97e62563c51459caeab
iwindd <freewinendlife@gmail.com>
2025-08-17 22:55:36 +0700

    feat(range): enhance date range selection with custom dialog and popover options

--- src/app/dashboard/range/index.tsx
+++ src/app/dashboard/range/index.tsx
@@ -1,73 +1,156 @@
 "use client";
 import { AnalyticsTwoTone } from "@mui/icons-material";
-import { Button, Card, CardHeader, Stack, Tooltip } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  MenuItem,
+  Modal,
+  Popover,
+  Stack,
+  Tooltip,
+} from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
 import dayjs, { Dayjs } from "dayjs";
 import React, { useEffect } from "react";
 import { enqueueSnackbar } from "notistack";
 import { RangeChange } from "@/actions/dashboard/range";
+import { usePopover } from "@/hooks/use-popover";
+import { date2 as date } from "@/libs/formatter";
 
-const Range = ({ savedStart, savedEnd }: {
-  savedStart: string | null,
-  savedEnd: string | null
+const Range = ({
+  savedStart,
+  savedEnd,
+}: {
+  savedStart: string | null;
+  savedEnd: string | null;
 }) => {
-  const [start, setStart] = React.useState<Dayjs | null>(savedStart ? dayjs(savedStart) : null);
-  const [end, setEnd] = React.useState<Dayjs | null>(savedEnd ? dayjs(savedStart) : null);
+  savedStart = savedStart || dayjs().subtract(1, "month").format();
+  savedEnd = savedEnd || dayjs().format();
+  const [start, setStart] = React.useState<Dayjs | null>(dayjs(savedStart));
+  const [end, setEnd] = React.useState<Dayjs | null>(dayjs(savedEnd));
+  const [isOpenCustomDialog, setIsOpenCustomDialog] = React.useState(false);
+  const popover = usePopover<HTMLButtonElement>();  
 
-  useEffect(() => {
-    if (savedStart ) setStart(dayjs(savedStart));
-    if (savedEnd ) setEnd(dayjs(savedEnd));
-  }, [savedStart, savedEnd, setStart, setEnd])
-
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const setDateRange = async (start?: Dayjs | null, end?: Dayjs | null) => {
     try {
+      start = start || dayjs().subtract(1, "month");
+      end = end || dayjs();
+      popover.handleClose();
+      setIsOpenCustomDialog(false);
+      setStart(start);
+      setEnd(end);
       await RangeChange(start?.format(), end?.format());
+      enqueueSnackbar("เปลี่ยนช่วงเวลาเรียบร้อยแล้ว!", { variant: "success" });
     } catch (error) {
-      setStart(dayjs().subtract(1, "month"));
-      setStart(dayjs());
-      enqueueSnackbar("เกิดข้อผิดพลาดกรุณาลองใหม่อีกครั้งภายหลัง", {
-        variant: "error",
-      });
+      setStart(dayjs(savedStart));
+      setEnd(dayjs(savedEnd));
+      enqueueSnackbar("ไม่สามารถเปลี่ยนช่วงเวลาได้", { variant: "error" });
     }
   };
 
+  const setDateRangeFromNow = (days: number) => {
+    popover.handleClose();
+    const now = dayjs();
+    setDateRange(now.subtract(days, "day"), now.endOf("day"));
+  };
+
   return (
-    <Card>
-      <CardHeader
-        title="ภาพรวม"
-        action={
-          <Stack
-            onSubmit={onSubmit}
-            component={"form"}
-            direction={"row"}
-            spacing={1}
-          >
-            <DatePicker
-              value={start}
-              name="start"
-              format="DD/MM/YYYY"
-              onChange={(data) => setStart(data)}
-              label="วันเริมต้น"
-              disableFuture
-            />
-            <DatePicker
-              value={end}
-              name="end"
-              format="DD/MM/YYYY"
-              onChange={(data) => setEnd(data)}
-              label="สิ้นสุด"
-              disableFuture
-            />
-            <Tooltip title="สรุปผล">
-              <Button type="submit" variant="contained">
-                <AnalyticsTwoTone />
-              </Button>
-            </Tooltip>
-          </Stack>
-        }
-      />
-    </Card>
+    <>
+      <Card>
+        <CardHeader
+          title="ภาพรวม"
+          subheader={`
+            ช่วงเวลา ${date(start?.toDate() || new Date())} 
+            ถึง ${date(end?.toDate() || new Date())}
+          `}
+          action={
+            <Stack
+              direction={"row"}
+              spacing={1}
+            >
+              <div>
+                <Tooltip title="สรุปผล">
+                  <Button
+                    variant="contained"
+                    onClick={popover.handleOpen}
+                    ref={popover.anchorRef}
+                    startIcon={<AnalyticsTwoTone />}
+                  >
+                    เลือกวันที่
+                  </Button>
+                </Tooltip>
+                <Popover
+                  anchorEl={popover.anchorRef.current}
+                  onClose={popover.handleClose}
+                  open={popover.open}
+                  anchorOrigin={{ horizontal: "left", vertical: "bottom" }}
+                >
+                  <MenuItem
+                    onClick={() =>
+                      setDateRange(dayjs().startOf("day"), dayjs().endOf("day"))
+                    }
+                  >
+                    วันนี้
+                  </MenuItem>
+                  <MenuItem onClick={() => setDateRangeFromNow(7)}>
+                    7 วันล่าสุด
+                  </MenuItem>
+                  <MenuItem onClick={() => setDateRangeFromNow(30)}>
+                    30 วันล่าสุด
+                  </MenuItem>
+                  <MenuItem onClick={() => {
+                    setIsOpenCustomDialog(true);
+                    popover.handleClose();
+                  }}>
+                    กำหนดเอง
+                  </MenuItem>
+                </Popover>
+              </div>
+            </Stack>
+          }
+        />
+      </Card>
+      <>
+        <Dialog
+          open={isOpenCustomDialog}
+          onClose={() => setIsOpenCustomDialog(false)}
+          maxWidth="xs"
+          fullWidth
+        >
+          <DialogTitle sx={{ m: 0, p: 2 }}>กำหนดช่วงเวลา</DialogTitle>
+          <DialogContent dividers>
+            <Stack spacing={2}>
+              <DatePicker
+                value={start}
+                name="start"
+                format="DD/MM/YYYY"
+                onChange={(data) => setStart(data)}
+                label="วันเริมต้น"
+                disableFuture
+              />
+              <DatePicker
+                value={end}
+                name="end"
+                format="DD/MM/YYYY"
+                onChange={(data) => setEnd(data)}
+                label="สิ้นสุด"
+                disableFuture
+              />
+            </Stack>
+          </DialogContent>
+          <DialogActions>
+            <Button autoFocus onClick={() => setIsOpenCustomDialog(false)}>ปิด</Button>
+            <Button autoFocus onClick={() => setDateRange(start, end)}>สรุปผล</Button>
+          </DialogActions>
+        </Dialog>
+      </>
+    </>
   );
 };
 
