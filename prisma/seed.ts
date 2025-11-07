4f25017a8d98f4c6a5ed9479409507c6132dd685
iwindd <freewinendlife@gmail.com>
2025-11-08 01:44:04 +0700

    Refactor code structure for improved readability and maintainability

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,74 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
+import _ from "lodash";
+import orders from "./data/orders.json";
 import permissions from "./data/permissions.json";
 import products from "./data/products.json";
 import stores from "./data/stores.json";
 
 const prisma = new PrismaClient();
 const DEFAULT_PASSWORD = "password";
+
+const getOrders = async () => {
+  const products = orders.flatMap((order) => order.products);
+  const uniqueProductSerials = Array.from(
+    new Set(products.map((p) => p.serial))
+  );
+  const productsInDb = await prisma.product.findMany({
+    where: {
+      serial: { in: uniqueProductSerials },
+    },
+    include: {
+      category: {
+        select: {
+          label: true,
+        },
+      },
+    },
+  });
+
+  const result = [];
+
+  for (const order of orders) {
+    const productsForOrder = productsInDb.filter((product) =>
+      order.products.some((p: any) => p.serial === product.serial)
+    );
+
+    if (productsForOrder.length === 0) continue;
+
+    result.push({
+      id: order.id,
+      price: order.price,
+      cost: order.cost,
+      profit: order.profit,
+      method: order.method as any,
+      type: order.type as any,
+      note: order.note,
+      created_at: new Date(order.created_at),
+      updated_at: new Date(order.updated_at),
+      store_id: order.store_id,
+      text: productsForOrder.map((p: any) => p.label).join(", "),
+      products: {
+        create: productsForOrder.map((product) => {
+          const data = order.products.find(
+            (p: any) => p.serial === product.serial
+          );
+
+          return {
+            serial: product.serial,
+            label: product.label,
+            price: data?.price || product.price,
+            cost: data?.cost || product.cost,
+            category: product.category?.label || "-",
+            count: data?.count || 1,
+          };
+        }),
+      },
+    });
+  }
+
+  return result;
+};
 
 async function main() {
   await prisma.$transaction(async () => {
@@ -114,6 +177,28 @@ async function main() {
     }
   });
 
+  const ordersData = await getOrders();
+
+  const chunks = _.chunk(ordersData, 100);
+
+  console.log(
+    `Seeding ${ordersData.length} orders in ${chunks.length} chunks...`
+  );
+
+  for (const [i, chunk] of chunks.entries()) {
+    await prisma.$transaction(async () => {
+      for (const order of chunk) {
+        const castedOrder = order as any;
+        await prisma.order.upsert({
+          where: { id: castedOrder.id },
+          update: {},
+          create: castedOrder,
+        });
+      }
+    });
+    console.log(`✅ Seeded chunk ${i + 1}/${chunks.length}`);
+  }
+
   console.log(
     `✅ Seed completed successfully!\n`,
     `--------------------------------\n`,
@@ -123,6 +208,8 @@ async function main() {
     `Created ${await prisma.user.count()} users (default password: ${DEFAULT_PASSWORD})\n`,
     `Created ${await prisma.product.count()} products\n`,
     `Created ${await prisma.category.count()} categories\n`,
+    `Created ${await prisma.order.count()} orders (${await prisma.orderProduct.count()} products)\n`,
+
     `--------------------------------\n`
   );
 }
