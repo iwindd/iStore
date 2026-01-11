6ad0657be846d7e8dbaf9269ad813cfd8ccc38b5
Achirawit Kaewkhong <freewinendlife@gmail.com>
2026-01-11 23:03:35 +0700

    Fix import path for r2 module in getPresignedUrl
    Updated the import statement to use '@/libs/r2' instead of '@/lib/r2' to correct the module path.

--- src/actions/upload/getPresignedUrl.ts
+++ src/actions/upload/getPresignedUrl.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { r2 } from "@/lib/r2";
+import { r2 } from "@/libs/r2";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { v4 as uuidv4 } from "uuid";
