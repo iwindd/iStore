1e87d38227a372ca1d8696f8a4ad98769c519d9f
Achirawit Kaewkhong <freewinendlife@gmail.com>
2026-01-16 08:05:15 +0700

    Add LoadingPage component with progress bar
    Introduces a new LoadingPage component using MUI's LinearProgress and Stack to display a centered loading indicator.

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import { LinearProgress, Stack } from "@mui/material";
+
+const LoadingPage = () => {
+  return (
+    <Stack mt={"50%"} px={"30%"}>
+      <LinearProgress />
+    </Stack>
+  );
+};
+
+export default LoadingPage;
