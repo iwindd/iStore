14f8b7355e7ea7a13c31d52437c4860b4cffd01e
Achirawit Kaewkhong <freewinendlife@gmail.com>
2026-02-01 00:33:46 +0700

    Remove Env.ts and global LineMessingApi
    Delete src/config/Env.ts and remove the import/use of the env-based LineMessingApi in src/libs/line.ts. The global MessagingApiClient constructed from static env tokens was eliminated, leaving getMessagingApiByKey to handle per-key client resolution (token retrieval now handled elsewhere).

--- src/config/Env.ts
+++
@@ -1,9 +0,0 @@
-// TODO: Move refactore all env to this file
-const env = {
-  line: {
-    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN!,
-    channelSecret: process.env.LINE_CHANNEL_SECRET!,
-  },
-};
-
-export default env;
--- src/libs/line.ts
+++ src/libs/line.ts
@@ -1,11 +1,6 @@
-import env from "@/config/Env";
 import * as line from "@line/bot-sdk";
 import db from "./db";
 export const runtime = "nodejs";
-
-export const LineMessingApi = new line.messagingApi.MessagingApiClient({
-  channelAccessToken: env.line.channelAccessToken,
-});
 
 export const getMessagingApiByKey = async (key: string) => {
   const application = await db.lineApplication.findUniqueOrThrow({
